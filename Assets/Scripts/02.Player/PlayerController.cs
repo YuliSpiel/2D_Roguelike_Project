91acdbcd84d937694685d614d4fe72ab4019bdf3
Jiyun <juliyooni@gmail.com>
2026-02-10 15:36:30 +0900

    [Fix] 빌드 에러 해결

--- Assets/Scripts/02.Player/PlayerController.cs
+++ Assets/Scripts/02.Player/PlayerController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SceneTemplate;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.PlayerLoop;
